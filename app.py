e6a019073b7e9b7acbe74a16372c9e2bdd9c7f9b
DevGuixavier <dev.guixavier@gmail.com>
2025-04-01 13:26:37 -0300

    update app.py
    adicionei o render template para puxar a tela principal

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from config import app, db
+from flask import render_template
 from Produtos.produtos_routes import produtos_blueprint
 from Clientes.clientes_routes import clientes_blueprint
 from Pedidos.pedidos_routes import pedidos_blueprint
@@ -12,6 +13,11 @@ app.register_blueprint(pedidos_blueprint)
 # Criar todas as tabelas no banco de dados
 with app.app_context():
     db.create_all()
+
+# Rota para a página principal (home)
+@app.route('/')
+def home():
+    return render_template('index.html')  # Substitua 'index.html' pelo nome do seu arquivo
 
 # Executar a aplicação
 if __name__ == '__main__':
